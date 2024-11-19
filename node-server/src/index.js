8707b6dd4cdd0feda978216880bc3764552d0094
Divyanshu Singh <divyanshusingh@gofynd.com>
2024-11-20 05:21:33 +0530

    redis password support added

--- node-server/src/index.js
+++ node-server/src/index.js
@@ -14,7 +14,8 @@ const startServer = async () => {
             socket: {
                 host: process.env.REDIS_HOST || "localhost",
                 port: process.env.REDIS_PORT || 6379,
-            }
+            },
+            password: process.env.REDIS_PASSWORD || ''
         });
         await redisClient.connect();
 
